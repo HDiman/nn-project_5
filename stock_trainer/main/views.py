6e37174aca5050381f466672542f2cb9b6b90d73
Dmitry Sannikov <HDiman@users.noreply.github.com>
2023-03-14 21:39:22 +0300

    NN Project_4 ver. 1.6 (Беру данные из базы данных)

--- stock_trainer/main/views.py
+++ stock_trainer/main/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
-
+from .models import Portfolio
 # Create your views here.
 
 
 def index(request):
-    return render(request, 'main/index.html')
+    item_1_title = Portfolio.objects.all()[0].title
+    item_1_num = Portfolio.objects.all()[0].num
+    item_2_title = Portfolio.objects.all()[1].title
+    item_2_num = Portfolio.objects.all()[1].num
+    return render(request, 'main/index.html', {'item1': item_1_title,
+                                               'item2': item_1_num,
+                                               'item3': item_2_title,
+                                               'item4': item_2_num})
 
 
