d4726088c84fd705315881fcc502256b434332a4
Dmitry Sannikov <HDiman@users.noreply.github.com>
2023-03-14 22:11:51 +0300

    NN Project_4 ver. 1.7 (Работа с базой данных)

--- stock_trainer/main/views.py
+++ stock_trainer/main/views.py
@@ -3,14 +3,28 @@ from .models import Portfolio
 # Create your views here.
 
 
+stock_price = 100
+bond_price = 1000
+
+def sum_1(num, price):
+    sum = num * price
+    return sum
+
+
 def index(request):
     item_1_title = Portfolio.objects.all()[0].title
     item_1_num = Portfolio.objects.all()[0].num
+    item_1_sum = sum_1(item_1_num, stock_price)
     item_2_title = Portfolio.objects.all()[1].title
     item_2_num = Portfolio.objects.all()[1].num
-    return render(request, 'main/index.html', {'item1': item_1_title,
-                                               'item2': item_1_num,
-                                               'item3': item_2_title,
-                                               'item4': item_2_num})
+    item_2_sum = sum_1(item_2_num, bond_price)
+    return render(request, 'main/index.html', {'item1_title': item_1_title,
+                                               'item1_num': item_1_num,
+                                               'item1_sum': item_1_sum,
+                                               'stock_price': stock_price,
+                                               'item2_title': item_2_title,
+                                               'item2_num': item_2_num,
+                                               'item2_sum': item_2_sum,
+                                               'bond_price': bond_price})
 
 
