156a2c97144de1d7baca15310860f571d7f847e3
Dmitry Sannikov <HDiman@users.noreply.github.com>
2023-03-17 23:03:33 +0300

    NN Project_4 ver. 2.03 (Сохранение данных перед началом обработки базы)

--- stock_trainer/main/views.py
+++ stock_trainer/main/views.py
@@ -9,10 +9,12 @@ import time
 
 start_capital = 100000.00
 cash = 0
+
 volatility = 0.2  # волатильность акции (стандартное отклонение ежемесячных процентных изменений цены)
 time_horizon = 120  # количество дней наблюдения
 
 
+# Блок по расчету новой цены акции и облигации
 def prices(stock_price, bond_price):
     monthly_return = math.exp(random.gauss(0.0, volatility)) - 1.0
     stock_price *= 1.0 + monthly_return
@@ -20,15 +22,15 @@ def prices(stock_price, bond_price):
     return stock_price, bond_price
 
 
-def breifcase(stock_num, bond_num, cash):
-    # Блок оценки стоимости портфеля
-    stock_case = round((stock_price * stock_num), 2)
-    bond_case = round((bond_price * bond_num), 2)
-    personal_case = round((stock_case + bond_case + cash), 2)
+# Блок оценки стоимости портфеля
+def briefcase(stock_num, bond_num, cash, stock_price, bond_price):
+    stock_case = round(stock_price * stock_num)
+    bond_case = round(bond_price * bond_num)
+    personal_case = round(stock_case + bond_case + cash)
     stock_interest = round(stock_case / (personal_case / 100))
     bond_interest = 100 - stock_interest
     cash += 20000
-
+    return stock_case, bond_case, personal_case, stock_interest, bond_interest
 
 # def upload(request):
 #     if request.method == 'POST':
@@ -51,15 +53,17 @@ def index(request):
     item_2_sum = item_2_num * item_2_price
     capital = item_1_sum + item_2_sum
 
-    return render(request, 'main/index.html', {'item1_title': item_1_title,
-                                               'item1_num': item_1_num,
-                                               'item1_sum': item_1_sum,
-                                               'item1_price': item_1_price,
-                                               'item2_title': item_2_title,
-                                               'item2_num': item_2_num,
-                                               'item2_sum': item_2_sum,
-                                               'item2_price': item_2_price,
-                                               'capital': capital,
-                                               'cash': cash})
+    data = {'item1_title': item_1_title,
+            'item1_num': item_1_num,
+            'item1_sum': item_1_sum,
+            'item1_price': item_1_price,
+            'item2_title': item_2_title,
+            'item2_num': item_2_num,
+            'item2_sum': item_2_sum,
+            'item2_price': item_2_price,
+            'capital': capital,
+            'cash': cash}
+
+    return render(request, 'main/index.html', context=data)
 
 
