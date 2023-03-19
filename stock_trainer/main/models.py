edc68cec12a95926fba768cec7e99f8ad82005cd
Dmitry Sannikov <HDiman@users.noreply.github.com>
2023-03-19 19:35:19 +0300

    NN Project_4 ver. 3.04 (Не работает. Нужно вернуться.)

--- stock_trainer/main/models.py
+++ stock_trainer/main/models.py
@@ -1,13 +1,12 @@
 from django.db import models
 
+
 # Create your models here.
-
-
 class Portfolio(models.Model):
     title = models.CharField('Название актива', max_length=100)
     num = models.IntegerField('Кол-во')
     price = models.IntegerField('Цена', null=True)
-    # month = models.IntegerField('Месяц', null=True)
+    month = models.IntegerField('Месяц', null=True)
 
     def __str__(self):
         return self.title
